e65b03ae3667de2c29381c844779851fe1079bad
ooohboy <trevicyber2@gmail.com>
2025-05-14 16:27:40 +0200

    Leap

---
+++ Leap.ps1
@@ -0,0 +1,39 @@
+function Test-LeapYear {
+    <#
+    .SYNOPSIS
+    Given a year, report if it is a leap year.
+    
+    .DESCRIPTION
+    Calculate whether the supplied year is a leap year. A leap year is determined from the following
+    calculation:
+
+    on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+    
+    .PARAMETER year
+    The year to test
+    
+    .EXAMPLE
+    Test-LeapYear -year 2018
+
+    Returns false
+
+    .EXAMPLE
+    Test-LeapYear -year 2020
+
+    Returns True
+    #>
+    param( [int]$year )
+    
+    if (($year % 4) -eq 0) {
+        if (($year % 100) -eq 0) {
+            if (($year % 400) -eq 0) {
+                return $true
+            }
+            return $false
+        }
+        return $true   
+    }
+    return $false
+}
